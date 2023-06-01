5ef02ad27ff2b28b6d4031879e2fc83a44c84ad6
Esteban Borai <estebanborai@gmail.com>
2023-06-01 18:54:29 -0400

    chore: initial commit

---
+++ src/routes/stories/Counter.story.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Counter from '$lib/components/Counter.svelte';
+
+  import type { Hst as Histoire } from '@histoire/plugin-svelte';
+
+  export let Hst: Histoire;
+</script>
+
+<Hst.Story>
+  <Counter />
+</Hst.Story>
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { HstSvelte } from '@histoire/plugin-svelte'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	histoire: {
+    plugins: [
+      HstSvelte(),
+    ],
+    setupFile: './src/histoire.setup.ts',
+    tree: {
+      groups: [
+        {
+          id: 'top',
+          title: '',
+        },
+      ],
+    },
+  },
+});
