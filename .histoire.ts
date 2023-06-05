e9f28892db51116aef868e539dfc2b7d21ab692b
Esteban Borai <estebanborai@gmail.com>
2023-06-04 20:35:15 -0400

    feat: use `hash` for `routerMode`

---
+++ .histoire.ts
@@ -0,0 +1,7 @@
+// https://github.com/histoire-dev/histoire/blob/main/docs/guide/config.md
+// https://github.com/histoire-dev/histoire/blob/main/docs/reference/config.md
+import { defineConfig } from 'histoire'
+
+export default defineConfig({
+  routerMode: 'hash',
+})
